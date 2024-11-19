d22eb9d88369016db69749383c1da47296fcad24
BValentin22 <ballavalentin18@gmail.com>
2024-11-19 09:29:34 +0200

    Fixed empty ID handling

--- bvim2209-web/src/main/java/edu/bbte/idde/bvim2209/web/servlet/json/ToDoJsonServlet.java
+++ bvim2209-web/src/main/java/edu/bbte/idde/bvim2209/web/servlet/json/ToDoJsonServlet.java
@@ -50,17 +50,19 @@ public class ToDoJsonServlet extends HttpServlet {
                         logger.error("Failed to write error response", ioException);
                     }
                 }
-            } catch (NumberFormatException exception) {
+            } catch (IllegalArgumentException exception) {
+                logger.warn("Invalid id: {} in query", id);
                 resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
                 try (PrintWriter writer = resp.getWriter()) {
-                    writer.write("{\"error\": \"Invalid id\"}");
+                    writer.write("{\"error\"" + exception.getMessage() + "\"}");
                 } catch (IOException ioException) {
                     logger.error("Failed to write error response", ioException);
                 }
             } catch (EntityNotFoundException exception) {
+                logger.warn("Entity with id: {} not found", id);
                 resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
                 try (PrintWriter writer = resp.getWriter()) {
-                    writer.write("{\"error\": \"To do not found\"}");
+                    writer.write("{\"error\"" + exception.getMessage() + "\"}");
                 } catch (IOException ioException) {
                     logger.error("Failed to write error response", ioException);
                 }
@@ -103,7 +105,10 @@ public class ToDoJsonServlet extends HttpServlet {
         logger.info("Executing doPut request");
         try {
             String idParam = req.getParameter("id");
-            handleEmptyID(idParam, resp);
+            if (idParam == null || idParam.isEmpty()) {
+                handleEmptyID(idParam, resp);
+                return;
+            }
             Long id = Long.parseLong(idParam);
             ToDo toDo = objectMapper.readValue(req.getReader(), ToDo.class);
             if (toDo.getId() != null) {
@@ -135,25 +140,16 @@ public class ToDoJsonServlet extends HttpServlet {
         }
     }
 
-    private static void handleEmptyID(String idParam, HttpServletResponse resp) {
-        if (idParam == null) {
-            logger.warn("Id has not been provided.");
-            resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
-            try (PrintWriter writer = resp.getWriter()) {
-                writer.write("{\"error\": \"" + "The `id` field should be provided." + "\"}");
-            } catch (IOException ioException) {
-                logger.error("Failed to write error response", ioException);
-            }
-        }
-    }
-
     @Override
     protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         resp.setContentType("application/json");
         logger.info("Executing doDelete request");
         try {
             String idParam = req.getParameter("id");
-            handleEmptyID(idParam, resp);
+            if (idParam == null || idParam.isBlank()) {
+                handleEmptyID(idParam, resp);
+                return;
+            }
             Long id = Long.parseLong(idParam);
             toDoService.deleteToDo(id);
             resp.getWriter().write("{\"message\": \"Entity with id '" + id + "' deleted successfully.\"}");
@@ -171,6 +167,16 @@ public class ToDoJsonServlet extends HttpServlet {
             } catch (IOException ioException) {
                 logger.error("Failed to write error response", ioException);
             }
+        }
+    }
+
+    private static void handleEmptyID(String idParam, HttpServletResponse resp) {
+        logger.warn("Id has not been provided.");
+        resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+        try (PrintWriter writer = resp.getWriter()) {
+            writer.write("{\"error\": \"" + "The `id` field should be provided." + "\"}");
+        } catch (IOException ioException) {
+            logger.error("Failed to write error response", ioException);
         }
     }
 }
