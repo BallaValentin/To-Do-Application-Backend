996dca9ada41dcdd474d1d001a9adf1195f01c3c
BValentin22 <ballavalentin18@gmail.com>
2024-11-19 10:11:03 +0200

    In toDoThymeleafServlet the collection of toDos is not converted to List<Map> anymore

--- bvim2209-web/src/main/java/edu/bbte/idde/bvim2209/web/servlet/thymeleaf/ToDoThymeleafServlet.java
+++ bvim2209-web/src/main/java/edu/bbte/idde/bvim2209/web/servlet/thymeleaf/ToDoThymeleafServlet.java
@@ -9,6 +9,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
@@ -25,25 +26,9 @@ public class ToDoThymeleafServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd", Locale.ENGLISH);
         Map<String, Object> model = new ConcurrentHashMap<>();
         Collection<ToDo> toDoCollection = toDoService.findAll();
-        List<Map<String, Object>> todosForView = new ArrayList<>();
-        for (ToDo toDo : toDoCollection) {
-            todosForView.add(formatToDoForView(toDo, dateFormat));
-        }
-        model.put("todos", todosForView);
-
+        model.put("todos", toDoCollection);
         ThymeleafEngineFactory.process(req, resp, "todos.html", model);
-    }
-
-    private Map<String, Object> formatToDoForView(ToDo todo, SimpleDateFormat dateFormat) {
-        Map<String, Object> formattedTodo = new ConcurrentHashMap<>();
-        formattedTodo.put("id", todo.getId());
-        formattedTodo.put("title", todo.getTitle());
-        formattedTodo.put("description", todo.getDescription());
-        formattedTodo.put("dueDate", dateFormat.format(todo.getDueDate())); // Format date as string
-        formattedTodo.put("levelOfImportance", todo.getLevelOfImportance());
-        return formattedTodo;
     }
 }
