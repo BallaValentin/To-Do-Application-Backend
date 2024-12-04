e67ad73935fbdfcd10074e5b8be25be6cdb9d402
BValentin22 <ballavalentin18@gmail.com>
2024-12-04 22:48:30 +0200

    Added jdbc configuration

--- bvim2209-spring/src/main/java/edu/bbte/idde/bvim2209/spring/backend/repo/jdbc/JdbcDao.java
+++ bvim2209-spring/src/main/java/edu/bbte/idde/bvim2209/spring/backend/repo/jdbc/JdbcDao.java
@@ -3,35 +3,131 @@ package edu.bbte.idde.bvim2209.spring.backend.repo.jdbc;
 import edu.bbte.idde.bvim2209.spring.backend.model.BaseEntity;
 import edu.bbte.idde.bvim2209.spring.backend.repo.Dao;
 import edu.bbte.idde.bvim2209.spring.exceptions.EntityNotFoundException;
-import org.springframework.context.annotation.Profile;
-import org.springframework.stereotype.Repository;
+import lombok.extern.slf4j.Slf4j;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.Collection;
-import java.util.List;
 
-public class JdbcDao<T extends BaseEntity> implements Dao<T> {
-    @Override
-    public Collection<T> findAll() {
-        return List.of();
+@Slf4j
+public abstract class JdbcDao<T extends BaseEntity> implements Dao<T> {
+    protected JdbcDao() {
     }
 
+    protected abstract T mapResultSetToEntity(ResultSet resultSet) throws SQLException;
+
+    protected abstract void setStatementForInsert(PreparedStatement preparedStatement, T entity) throws SQLException;
+
+    protected abstract void setStatementForUpdate(PreparedStatement preparedStatement, T entity) throws SQLException;
+
+    protected abstract PreparedStatement prepareStatementForFindAll() throws SQLException;
+
+    protected abstract PreparedStatement prepareStatementForInsert() throws SQLException;
+
+    protected abstract PreparedStatement prepareStatementForFindById(Long id) throws SQLException;
+
+    protected abstract PreparedStatement prepareStatementForDeleteById(Long id) throws SQLException;
+
+    protected abstract PreparedStatement prepareStatementForUpdate() throws SQLException;
+
+    protected abstract Integer getPrimaryKeyIndex();
+
+    protected abstract Integer getNumberOfColumnsToUpdate();
+
     @Override
-    public T findById(Long id) throws EntityNotFoundException {
-        return null;
+    public Collection<T> findAll() {
+        log.info("Trying to fetch all entities from database");
+        Collection<T> entities = new ArrayList<>();
+        try (
+                PreparedStatement preparedStatement = prepareStatementForFindAll();
+                ResultSet resultSet = preparedStatement.executeQuery()
+        ) {
+            while (resultSet.next()) {
+                T entity = mapResultSetToEntity(resultSet);
+                entities.add(entity);
+            }
+        } catch (SQLException exception) {
+            log.error("Error fetching all entities from database", exception);
+        }
+        log.info("All entities have been successfully fetched from database");
+        return entities;
     }
 
     @Override
     public void create(T entity) throws IllegalArgumentException {
+        log.info("Trying to insert new entity in database");
+        try (
+                PreparedStatement preparedStatement = prepareStatementForInsert()
+        ) {
+            setStatementForInsert(preparedStatement, entity);
+            int rowsAffected = preparedStatement.executeUpdate();
+            if (rowsAffected > 0) {
+                try (ResultSet generatedKeys = preparedStatement.getGeneratedKeys()) {
+                    if (generatedKeys.next()) {
+                        entity.setId(generatedKeys.getLong(getPrimaryKeyIndex()));
+                    }
+                }
+            }
+        } catch (SQLException exception) {
+            log.error("Error creating entity in database", exception);
+            throw new IllegalArgumentException("Could not create entity", exception);
+        }
+        log.info("New entity has been successfully inserted into database");
+    }
 
+    @Override
+    public T findById(Long id) throws EntityNotFoundException {
+        log.info("Trying to find entity by id: {} in database", id);
+        try (
+                PreparedStatement preparedStatement = prepareStatementForFindById(id)) {
+            preparedStatement.setLong(getPrimaryKeyIndex(), id);
+            try (ResultSet resultSet = preparedStatement.executeQuery()) {
+                if (resultSet.next()) {
+                    log.info("Entity with id {} has been successfully found in database", id);
+                    return mapResultSetToEntity(resultSet);
+                } else {
+                    throw new EntityNotFoundException("Entity with ID " + id + " not found");
+                }
+            }
+        } catch (SQLException exception) {
+            log.error("Error finding entity by ID in database", exception);
+            throw new EntityNotFoundException("Entity with ID " + id + " not found.");
+        }
     }
 
     @Override
     public void update(T entity) throws EntityNotFoundException {
-
+        log.info("Trying to update entity with id {} in database", entity.getId());
+        try (
+                PreparedStatement preparedStatement = prepareStatementForUpdate()) {
+            setStatementForUpdate(preparedStatement, entity);
+            preparedStatement.setLong(getNumberOfColumnsToUpdate() + 1, entity.getId());
+            int rowsAffected = preparedStatement.executeUpdate();
+            if (rowsAffected == 0) {
+                throw new EntityNotFoundException("Entity with ID " + entity.getId() + " not found.");
+            }
+        } catch (SQLException exception) {
+            log.error("Error updating entity in database", exception);
+        }
+        log.info("Entity with id {} has been successfully updated in database", entity.getId());
     }
 
     @Override
     public void delete(Long id) throws EntityNotFoundException {
+        log.info("Trying to delete entity with id {} from database", id);
+        try (
+                PreparedStatement preparedStatement = prepareStatementForDeleteById(id)) {
+            preparedStatement.setLong(1, id);
+            int rowsAffected = preparedStatement.executeUpdate();
 
+            if (rowsAffected == 0) {
+                throw new EntityNotFoundException("Entity with ID " + id + " not found.");
+            }
+        } catch (SQLException e) {
+            log.error("Error deleting entity from database", e);
+        }
+        log.info("Entity with id {} has been successfully deleted from database", id);
     }
 }
--- bvim2209-spring/src/main/java/edu/bbte/idde/bvim2209/spring/backend/repo/jdbc/ToDoJdbcDao.java
+++ bvim2209-spring/src/main/java/edu/bbte/idde/bvim2209/spring/backend/repo/jdbc/ToDoJdbcDao.java
@@ -2,10 +2,110 @@ package edu.bbte.idde.bvim2209.spring.backend.repo.jdbc;
 
 import edu.bbte.idde.bvim2209.spring.backend.model.ToDo;
 import edu.bbte.idde.bvim2209.spring.backend.repo.ToDoDao;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Repository;
 
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 @Repository
 @Profile("jdbc")
+@Slf4j
 public class ToDoJdbcDao extends JdbcDao<ToDo> implements ToDoDao {
+    private final DataSource dataSource;
+
+    @Autowired
+    public ToDoJdbcDao(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    @Override
+    protected ToDo mapResultSetToEntity(ResultSet resultSet) throws SQLException {
+        log.info("Mapping ResultSet to ToDo entity.");
+        ToDo toDo = new ToDo();
+        toDo.setId(resultSet.getLong("ID"));
+        toDo.setTitle(resultSet.getString("Title"));
+        toDo.setDescription(resultSet.getString("Description"));
+        toDo.setDueDate(resultSet.getDate("DueDate"));
+        toDo.setLevelOfImportance(resultSet.getInt("ImportanceLevel"));
+        return toDo;
+    }
+
+    @Override
+    protected void setStatementForInsert(PreparedStatement preparedStatement, ToDo entity) throws SQLException {
+        log.info("Setting parameters for PreparedStatement to insert ToDo entity");
+        preparedStatement.setString(1, entity.getTitle());
+        preparedStatement.setString(2, entity.getDescription());
+        preparedStatement.setDate(3, new java.sql.Date(entity.getDueDate().getTime()));
+        preparedStatement.setInt(4, entity.getLevelOfImportance());
+    }
+
+    @Override
+    protected void setStatementForUpdate(PreparedStatement preparedStatement, ToDo entity) throws SQLException {
+        log.info("Setting parameters for PreparedStatement to update ToDo entity");
+        preparedStatement.setString(1, entity.getTitle());
+        preparedStatement.setString(2, entity.getDescription());
+        preparedStatement.setDate(3, new java.sql.Date(entity.getDueDate().getTime()));
+        preparedStatement.setInt(4, entity.getLevelOfImportance());
+    }
+
+    @Override
+    protected PreparedStatement prepareStatementForFindAll() throws SQLException {
+        String query = "SELECT * FROM ToDo";
+        Connection connection = dataSource.getConnection();
+        PreparedStatement preparedStatement;
+        preparedStatement = connection.prepareStatement(query);
+        return preparedStatement;
+    }
+
+    @Override
+    protected PreparedStatement prepareStatementForInsert() throws SQLException {
+        String query = "INSERT INTO ToDo (Title, Description, DueDate, ImportanceLevel) VALUES (?, ?, ?, ?)";
+        Connection connection = dataSource.getConnection();
+        PreparedStatement preparedStatement;
+        preparedStatement = connection.prepareStatement(query, PreparedStatement.RETURN_GENERATED_KEYS);
+        return preparedStatement;
+    }
+
+    @Override
+    protected PreparedStatement prepareStatementForFindById(Long id) throws SQLException {
+        String query = "SELECT * FROM ToDo WHERE ID=?";
+        Connection connection = dataSource.getConnection();
+        PreparedStatement preparedStatement;
+        preparedStatement = connection.prepareStatement(query);
+        return preparedStatement;
+    }
+
+    @Override
+    protected PreparedStatement prepareStatementForDeleteById(Long id) throws SQLException {
+        String query = "DELETE FROM ToDo WHERE ID=?";
+        Connection connection = dataSource.getConnection();
+        PreparedStatement preparedStatement;
+        preparedStatement = connection.prepareStatement(query);
+        return preparedStatement;
+    }
+
+    @Override
+    protected PreparedStatement prepareStatementForUpdate() throws SQLException {
+        String query = "UPDATE ToDo SET Title=?, Description=?, DueDate=?, ImportanceLevel=? WHERE ID=?";
+        Connection connection = dataSource.getConnection();
+        PreparedStatement preparedStatement;
+        preparedStatement = connection.prepareStatement(query);
+        return preparedStatement;
+    }
+
+    @Override
+    protected Integer getPrimaryKeyIndex() {
+        return 1;
+    }
+
+    @Override
+    protected Integer getNumberOfColumnsToUpdate() {
+        return 4;
+    }
 }
