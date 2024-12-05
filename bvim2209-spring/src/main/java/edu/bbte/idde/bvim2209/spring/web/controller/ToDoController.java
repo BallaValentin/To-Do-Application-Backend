be4c31773eb143dc8609fc9fac01d1894ad75b9b
BValentin22 <ballavalentin18@gmail.com>
2024-12-05 19:51:20 +0200

    Added Get method for all todos in rest controller

---
+++ bvim2209-spring/src/main/java/edu/bbte/idde/bvim2209/spring/web/controller/ToDoController.java
@@ -0,0 +1,33 @@
+package edu.bbte.idde.bvim2209.spring.web.controller;
+
+import edu.bbte.idde.bvim2209.spring.backend.model.ToDo;
+import edu.bbte.idde.bvim2209.spring.backend.services.ToDoService;
+import edu.bbte.idde.bvim2209.spring.web.dto.ToDoDto;
+import edu.bbte.idde.bvim2209.spring.web.mapper.ToDoMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collection;
+
+@RestController
+@RequestMapping("/api/todos")
+public class ToDoController {
+    ToDoMapper toDoMapper;
+    ToDoService toDoService;
+
+    @Autowired
+    public ToDoController(ToDoMapper toDoMapper, ToDoService toDoService)
+    {
+        this.toDoMapper = toDoMapper;
+        this.toDoService = toDoService;
+    }
+
+    @GetMapping()
+    public Collection<ToDoDto> findAllToDos()
+    {
+        Collection<ToDo> todos = toDoService.findAll();
+        return toDoMapper.modelsToDto(todos);
+    }
+}
