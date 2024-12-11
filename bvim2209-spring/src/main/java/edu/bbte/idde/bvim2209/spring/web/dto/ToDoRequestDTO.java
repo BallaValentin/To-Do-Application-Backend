aa55fe894231baaf5af28d6e1f38cb1de1e2dd83
BValentin22 <ballavalentin18@gmail.com>
2024-12-11 16:57:10 +0200

    Using spring boot starter validation

--- bvim2209-spring/src/main/java/edu/bbte/idde/bvim2209/spring/web/dto/ToDoRequestDTO.java
+++ bvim2209-spring/src/main/java/edu/bbte/idde/bvim2209/spring/web/dto/ToDoRequestDTO.java
@@ -3,6 +3,7 @@ package edu.bbte.idde.bvim2209.spring.web.dto;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.Data;
 
 import java.util.Date;
@@ -11,10 +12,10 @@ import java.util.Date;
 public class ToDoRequestDTO {
     private Long id;
 
-    @NotBlank(message = "Title cannot be empty")
+    @NotBlank(message = "Title cannot be blank")
     private String title;
 
-    @NotBlank(message = "Description cannot be empty")
+    @NotBlank(message = "Description cannot be blank")
     private String description;
 
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
@@ -22,5 +23,6 @@ public class ToDoRequestDTO {
     private Date dueDate;
 
     @NotNull(message = "LevelOfImportance cannot be null")
+    @Positive(message = "LevelOfImportance level must be positive")
     private Integer levelOfImportance;
 }
