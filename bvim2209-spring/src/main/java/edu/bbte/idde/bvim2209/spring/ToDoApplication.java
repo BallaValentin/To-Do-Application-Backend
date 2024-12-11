ee12691705b78aed61d8e6ef7a140d7ac1a87d54
BValentin22 <ballavalentin18@gmail.com>
2024-12-11 17:00:43 +0200

    Removed desktip client from spring application

--- bvim2209-spring/src/main/java/edu/bbte/idde/bvim2209/spring/ToDoApplication.java
+++ bvim2209-spring/src/main/java/edu/bbte/idde/bvim2209/spring/ToDoApplication.java
@@ -1,13 +1,11 @@
 package edu.bbte.idde.bvim2209.spring;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 
-@SpringBootApplication(scanBasePackages = "edu.bbte.idde.bvim2209")
+@SpringBootApplication
 public class ToDoApplication {
     public static void main(String[] args) {
-        new SpringApplicationBuilder(ToDoApplication.class)
-                .headless(false)
-                .run(args);
+        SpringApplication.run(ToDoApplication.class, args);
     }
 }
