d6157fd62f6e3f293a87a54342012b6ff76a4139
BValentin22 <ballavalentin18@gmail.com>
2025-01-19 10:41:58 +0200

    Added cache config

---
+++ bvim2209-spring/src/main/java/edu/bbte/idde/bvim2209/spring/backend/config/CacheConfig.java
@@ -0,0 +1,24 @@
+package edu.bbte.idde.bvim2209.spring.backend.config;
+
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+
+import java.time.Duration;
+
+@Configuration
+@EnableCaching
+public class CacheConfig {
+    @Bean
+    public RedisCacheManager redisCacheManager(RedisConnectionFactory redisConnectionFactory) {
+        RedisCacheConfiguration config = RedisCacheConfiguration.defaultCacheConfig()
+                .entryTtl(Duration.ofMinutes(10))
+                .disableCachingNullValues();
+
+        return RedisCacheManager.builder(redisConnectionFactory).cacheDefaults(config).build();
+
+    }
+}
