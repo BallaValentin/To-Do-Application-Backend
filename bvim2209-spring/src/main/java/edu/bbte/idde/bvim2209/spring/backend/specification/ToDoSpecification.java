a858efc09cafa9c2be80a8b1a2b45870cdb92657
BValentin22 <ballavalentin18@gmail.com>
2025-01-22 17:06:02 +0200

    Added fixes to date specification

--- bvim2209-spring/src/main/java/edu/bbte/idde/bvim2209/spring/backend/specification/ToDoSpecification.java
+++ bvim2209-spring/src/main/java/edu/bbte/idde/bvim2209/spring/backend/specification/ToDoSpecification.java
@@ -8,18 +8,18 @@ import java.util.Date;
 public class ToDoSpecification {
     public static Specification<ToDo> withPriority(Integer priority) {
         return (root, query, criteriaBuilder) ->
-                priority != null ? criteriaBuilder.equal(root.get("priority"), priority) : null;
+                priority != null ? criteriaBuilder.equal(root.get("levelOfImportance"), priority) : null;
     }
 
     public static Specification<ToDo> withDueDateBefore(Date dueDate) {
         return (root, query, criteriaBuilder) ->
                 dueDate != null
-                        ? criteriaBuilder.greaterThanOrEqualTo(root.get("dueDate"), dueDate) : null;
+                        ? criteriaBuilder.lessThanOrEqualTo(root.get("dueDate"), dueDate) : null;
     }
 
     public static Specification<ToDo> withDueDateAfter(Date dueDate) {
         return (root, query, criteriaBuilder) ->
                 dueDate != null
-                        ? criteriaBuilder.lessThanOrEqualTo(root.get("dueDate"), dueDate) : null;
+                        ? criteriaBuilder.greaterThanOrEqualTo(root.get("dueDate"), dueDate) : null;
     }
 }
