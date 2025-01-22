97380343802e007ca8e87ebf2f041cae4a5d30a1
BValentin22 <ballavalentin18@gmail.com>
2025-01-22 16:41:33 +0200

    Added specifications for todo

---
+++ bvim2209-spring/src/main/java/edu/bbte/idde/bvim2209/spring/backend/specification/ToDoSpecification.java
@@ -0,0 +1,25 @@
+package edu.bbte.idde.bvim2209.spring.backend.specification;
+
+import edu.bbte.idde.bvim2209.spring.backend.model.ToDo;
+import org.springframework.data.jpa.domain.Specification;
+
+import java.util.Date;
+
+public class ToDoSpecification {
+    public static Specification<ToDo> withPriority(Integer priority) {
+        return (root, query, criteriaBuilder) ->
+                priority != null ? criteriaBuilder.equal(root.get("priority"), priority) : null;
+    }
+
+    public static Specification<ToDo> withDueDateBefore(Date dueDate) {
+        return (root, query, criteriaBuilder) ->
+                dueDate != null ?
+                        criteriaBuilder.greaterThanOrEqualTo(root.get("dueDate"), dueDate) : null;
+    }
+
+    public static Specification<ToDo> withDueDateAfetr(Date dueDate) {
+        return (root, query, criteriaBuilder) ->
+                dueDate != null ?
+                criteriaBuilder.lessThanOrEqualTo(root.get("dueDate"), dueDate) : null;
+    }
+}
