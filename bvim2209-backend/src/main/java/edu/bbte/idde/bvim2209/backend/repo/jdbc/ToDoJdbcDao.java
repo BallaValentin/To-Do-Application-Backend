baa0b144332f37df6f2b281cd6b3bfb7274d6f8b
BValentin22 <ballavalentin18@gmail.com>
2025-01-31 23:19:24 +0200

    Updated JdbcDao

--- bvim2209-backend/src/main/java/edu/bbte/idde/bvim2209/backend/repo/jdbc/ToDoJdbcDao.java
+++ bvim2209-backend/src/main/java/edu/bbte/idde/bvim2209/backend/repo/jdbc/ToDoJdbcDao.java
@@ -5,16 +5,23 @@ import edu.bbte.idde.bvim2209.backend.repo.ToDoDao;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
 
 public class ToDoJdbcDao extends JdbcDao<ToDo> implements ToDoDao {
 
     private static final Logger logger = LoggerFactory.getLogger(ToDoJdbcDao.class);
+
+    protected ToDoJdbcDao() {
+        super("ToDo", "ID", 1,
+                new ArrayList<>(Arrays.asList("Title", "Description", "DueDate", "ImportanceLevel")
+                ));
+    }
 
     @Override
     public Collection<ToDo> findByTitle(String title) {
@@ -51,58 +58,4 @@ public class ToDoJdbcDao extends JdbcDao<ToDo> implements ToDoDao {
         preparedStatement.setInt(4, entity.getLevelOfImportance());
     }
 
-    @Override
-    protected PreparedStatement prepareStatementForFindAll() throws SQLException {
-        String query = "SELECT * FROM ToDo";
-        Connection connection = dataSource.getConnection();
-        PreparedStatement preparedStatement;
-        preparedStatement = connection.prepareStatement(query);
-        return preparedStatement;
-    }
-
-    @Override
-    protected PreparedStatement prepareStatementForInsert() throws SQLException {
-        String query = "INSERT INTO ToDo (Title, Description, DueDate, ImportanceLevel) VALUES (?, ?, ?, ?)";
-        Connection connection = dataSource.getConnection();
-        PreparedStatement preparedStatement;
-        preparedStatement = connection.prepareStatement(query, PreparedStatement.RETURN_GENERATED_KEYS);
-        return preparedStatement;
-    }
-
-    @Override
-    protected PreparedStatement prepareStatementForFindById(Long id) throws SQLException {
-        String query = "SELECT * FROM ToDo WHERE ID=?";
-        Connection connection = dataSource.getConnection();
-        PreparedStatement preparedStatement;
-        preparedStatement = connection.prepareStatement(query);
-        return preparedStatement;
-    }
-
-    @Override
-    protected PreparedStatement prepareStatementForDeleteById(Long id) throws SQLException {
-        String query = "DELETE FROM ToDo WHERE ID=?";
-        Connection connection = dataSource.getConnection();
-        PreparedStatement preparedStatement;
-        preparedStatement = connection.prepareStatement(query);
-        return preparedStatement;
-    }
-
-    @Override
-    protected PreparedStatement prepareStatementForUpdate() throws SQLException {
-        String query = "UPDATE ToDo SET Title=?, Description=?, DueDate=?, ImportanceLevel=? WHERE ID=?";
-        Connection connection = dataSource.getConnection();
-        PreparedStatement preparedStatement;
-        preparedStatement = connection.prepareStatement(query);
-        return preparedStatement;
-    }
-
-    @Override
-    protected Integer getPrimaryKeyIndex() {
-        return 1;
-    }
-
-    @Override
-    protected Integer getNumberOfColumnsToUpdate() {
-        return 4;
-    }
 }
