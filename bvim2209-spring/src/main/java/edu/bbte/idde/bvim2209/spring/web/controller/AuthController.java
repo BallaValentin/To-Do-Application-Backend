ee96e3dc8aa5178501f399f29978b2ec48770042
BValentin22 <ballavalentin18@gmail.com>
2025-02-10 19:46:27 +0200

    Fixed gradle check issues

--- bvim2209-spring/src/main/java/edu/bbte/idde/bvim2209/spring/web/controller/AuthController.java
+++ bvim2209-spring/src/main/java/edu/bbte/idde/bvim2209/spring/web/controller/AuthController.java
@@ -23,8 +23,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.UUID;
-
 @RestController
 @RequestMapping("/api/auth")
 @CrossOrigin(value = "http://localhost:5173", allowCredentials = "true")
