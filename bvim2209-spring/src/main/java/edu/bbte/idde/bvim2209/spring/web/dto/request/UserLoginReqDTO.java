ad7e9dcd3440c2d7dec6e943fb04a2d1d92db0e6
BValentin22 <ballavalentin18@gmail.com>
2025-10-01 15:35:18 +0300

    Controller will only accept LoginRequest if remember is not null

--- bvim2209-spring/src/main/java/edu/bbte/idde/bvim2209/spring/web/dto/request/UserLoginReqDTO.java
+++ bvim2209-spring/src/main/java/edu/bbte/idde/bvim2209/spring/web/dto/request/UserLoginReqDTO.java
@@ -1,6 +1,7 @@
 package edu.bbte.idde.bvim2209.spring.web.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
@@ -9,5 +10,6 @@ public class UserLoginReqDTO {
     private String username;
     @NotBlank(message = "Password cannot be blank")
     private String password;
+    @NotNull(message = "Remember me must have a value")
     private Boolean rememberMe;
 }
